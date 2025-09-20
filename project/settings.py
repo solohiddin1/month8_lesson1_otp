5089eadb012899b507ba1cb51d4794274fc78503
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-09-20 16:17:53 +0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -133,4 +133,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sirojiddinovsolohiddin961@gmail.com'
-EMAIL_HOST_PASSWORD = 'tqhe kcdy oemn srwy'
+EMAIL_HOST_PASSWORD = ''
